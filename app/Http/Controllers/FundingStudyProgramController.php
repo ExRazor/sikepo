dc76876cc6474e60ab51c5887c5c56523b7d2c45
ExRazor <keroro.gunso97@gmail.com>
2021-02-17 02:46:30 +0800

    Perbaikan variabel $dana missing

--- app/Http/Controllers/FundingStudyProgramController.php
+++ app/Http/Controllers/FundingStudyProgramController.php
@@ -40,98 +40,97 @@ class FundingStudyProgramController extends Controller
         //             ->groupBy('status')
         //             ->get();
 
-        if(Auth::user()->hasRole('kaprodi')) {
+        if (Auth::user()->hasRole('kaprodi')) {
             $data         = FundingStudyProgram::groupBy('kd_dana')
-                                                ->groupBy('kd_prodi')
-                                                ->groupBy('id_ta')
-                                                ->where('kd_prodi',Auth::user()->kd_prodi)
-                                                ->orderBy('id_ta','desc')
-                                                ->select('kd_dana','kd_prodi','id_ta')
-                                                ->get();
+                ->groupBy('kd_prodi')
+                ->groupBy('id_ta')
+                ->where('kd_prodi', Auth::user()->kd_prodi)
+                ->orderBy('id_ta', 'desc')
+                ->select('kd_dana', 'kd_prodi', 'id_ta')
+                ->get();
         } else {
             $data         = FundingStudyProgram::groupBy('kd_dana')
-                                                ->groupBy('kd_prodi')
-                                                ->groupBy('id_ta')
-                                                ->orderBy('id_ta','desc')
-                                                ->select('kd_dana','kd_prodi','id_ta')
-                                                ->get();
+                ->groupBy('kd_prodi')
+                ->groupBy('id_ta')
+                ->orderBy('id_ta', 'desc')
+                ->select('kd_dana', 'kd_prodi', 'id_ta')
+                ->get();
         }
 
         $funding      = FundingStudyProgram::groupBy('kd_dana')->get('kd_dana');
 
-        foreach($funding as $f) {
+        $dana = array();
+        foreach ($funding as $f) {
             $dana[$f->kd_dana]  = DB::table('funding_study_programs as study')
-                                    ->select(
-                                        DB::raw('sum(study.nominal) as total'),
-                                        'funding_categories.jenis'
-                                    )
-                                    ->join('funding_categories','funding_categories.id', '=', 'study.id_kategori')
-                                    // ->where('funding_categories.jenis','!=',null)
-                                    ->where('study.kd_dana',$f->kd_dana)
-                                    ->groupBy('funding_categories.jenis')
-                                    ->get();
+                ->select(
+                    DB::raw('sum(study.nominal) as total'),
+                    'funding_categories.jenis'
+                )
+                ->join('funding_categories', 'funding_categories.id', '=', 'study.id_kategori')
+                // ->where('funding_categories.jenis','!=',null)
+                ->where('study.kd_dana', $f->kd_dana)
+                ->groupBy('funding_categories.jenis')
+                ->get();
         }
 
-        return view('funding.study-program.index',compact(['data','dana']));
+        return view('funding.study-program.index', compact(['data', 'dana']));
     }
 
     public function show($kd_dana)
     {
         $kd = decrypt($kd_dana);
 
-        $data     = FundingStudyProgram::where('kd_dana',$kd)->first();
-        $category = FundingCategory::with('children')->whereNull('id_parent')->orderBy('id','asc')->get();
+        $data     = FundingStudyProgram::where('kd_dana', $kd)->first();
+        $category = FundingCategory::with('children')->whereNull('id_parent')->orderBy('id', 'asc')->get();
 
         $categoryFilter = FundingCategory::all();
-        foreach($categoryFilter as $c) {
-            $nominal[$c->id] = FundingStudyProgram::where('kd_dana',$kd)->where('id_kategori',$c->id)->first();
+        foreach ($categoryFilter as $c) {
+            $nominal[$c->id] = FundingStudyProgram::where('kd_dana', $kd)->where('id_kategori', $c->id)->first();
         }
 
         $total  = DB::table('funding_study_programs as study')
-                    ->select(
-                        DB::raw('sum(study.nominal) as total')
-                    )
-                    ->where('study.kd_dana',$kd)
-                    ->first()
-                    ->total;
+            ->select(
+                DB::raw('sum(study.nominal) as total')
+            )
+            ->where('study.kd_dana', $kd)
+            ->first()
+            ->total;
 
-        return view('funding.study-program.show',compact(['data','nominal','category','total']));
-
+        return view('funding.study-program.show', compact(['data', 'nominal', 'category', 'total']));
     }
 
     public function create()
     {
-        $studyProgram = StudyProgram::where('kd_jurusan',setting('app_department_id'))->get();
-        $category     = FundingCategory::with('children')->orderBy('id','asc')->whereNull('id_parent')->get();
-        $academicYear = AcademicYear::where('semester','Ganjil')->orderBy('tahun_akademik','desc')->get();
+        $studyProgram = StudyProgram::where('kd_jurusan', setting('app_department_id'))->get();
+        $category     = FundingCategory::with('children')->orderBy('id', 'asc')->whereNull('id_parent')->get();
+        $academicYear = AcademicYear::where('semester', 'Ganjil')->orderBy('tahun_akademik', 'desc')->get();
 
-        return view('funding.study-program.form',compact(['studyProgram','category','academicYear']));
+        return view('funding.study-program.form', compact(['studyProgram', 'category', 'academicYear']));
     }
 
     public function edit($id)
     {
         $id = decrypt($id);
 
-        $category     = FundingCategory::with('children')->orderBy('id','asc')->whereNull('id_parent')->get();
-        $studyProgram = StudyProgram::where('kd_jurusan',setting('app_department_id'))->get();
-        $academicYear = AcademicYear::where('semester','Ganjil')->orderBy('tahun_akademik','desc')->get();
+        $category     = FundingCategory::with('children')->orderBy('id', 'asc')->whereNull('id_parent')->get();
+        $studyProgram = StudyProgram::where('kd_jurusan', setting('app_department_id'))->get();
+        $academicYear = AcademicYear::where('semester', 'Ganjil')->orderBy('tahun_akademik', 'desc')->get();
 
-        $categoryFilter = FundingCategory::with('children')->orderBy('id','asc')->get();
+        $categoryFilter = FundingCategory::with('children')->orderBy('id', 'asc')->get();
 
-        foreach($categoryFilter as $c) {
-            $q = FundingStudyProgram::where('kd_dana',$id)->where('id_kategori',$c->id);
+        foreach ($categoryFilter as $c) {
+            $q = FundingStudyProgram::where('kd_dana', $id)->where('id_kategori', $c->id);
             $cek = $q->exists();
-            if($cek) {
+            if ($cek) {
                 $nominal[$c->id] = $q->first()->nominal;
             }
         }
 
-        $data  = FundingStudyProgram::with('studyProgram','academicYear')->where('kd_dana',$id)->first();
+        $data  = FundingStudyProgram::with('studyProgram', 'academicYear')->where('kd_dana', $id)->first();
 
         // dd($nominal);
 
-        return view('funding.study-program.form',compact(['studyProgram','academicYear','category','data','nominal']));
-
+        return view('funding.study-program.form', compact(['studyProgram', 'academicYear', 'category', 'data', 'nominal']));
     }
 
 
@@ -140,10 +139,10 @@ class FundingStudyProgramController extends Controller
         DB::beginTransaction();
         try {
             //Init Kode Dana
-            $kd_dana = 'pd'.$request->kd_prodi.'_thn'.$request->id_ta;
+            $kd_dana = 'pd' . $request->kd_prodi . '_thn' . $request->id_ta;
 
             //Query
-            foreach($request->id_kategori as $index => $value) {
+            foreach ($request->id_kategori as $index => $value) {
                 $nominal = ($value) ? $value : '0';
 
                 $query                 = new FundingStudyProgram;
@@ -158,18 +157,17 @@ class FundingStudyProgramController extends Controller
             //Activity Log
             $property = [
                 'id'    => $query->kd_dana,
-                'name'  => $query->studyProgram->nama.' - '.$query->academicYear->tahun_akademik,
-                'url'   => route('funding.study-program.show',encrypt($query->kd_dana))
+                'name'  => $query->studyProgram->nama . ' - ' . $query->academicYear->tahun_akademik,
+                'url'   => route('funding.study-program.show', encrypt($query->kd_dana))
             ];
-            $this->log('created','Keuangan Program Studi',$property);
+            $this->log('created', 'Keuangan Program Studi', $property);
 
             DB::commit();
             return redirect()->route('funding.study-program')->with('flash.message', 'Data berhasil ditambahkan!')->with('flash.class', 'success');
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             DB::rollback();
             return redirect()->back()->with('flash.message', $e->getMessage())->with('flash.class', 'danger')->withInput($request->input());
         }
-
     }
 
 
@@ -181,10 +179,10 @@ class FundingStudyProgramController extends Controller
             $kd_dana = decrypt($request->_id);
 
             //Query
-            foreach($request->id_kategori as $index => $value) {
+            foreach ($request->id_kategori as $index => $value) {
                 $nominal = ($value) ? $value : '0';
 
-                $query                 = FundingStudyProgram::where('kd_dana',$kd_dana)->where('id_kategori',$index)->first();
+                $query                 = FundingStudyProgram::where('kd_dana', $kd_dana)->where('id_kategori', $index)->first();
                 $query->kd_prodi       = $request->kd_prodi;
                 $query->id_ta          = $request->id_ta;
                 $query->nominal        = str_replace(".", "", $nominal);
@@ -194,24 +192,23 @@ class FundingStudyProgramController extends Controller
             //Activity Log
             $property = [
                 'id'    => $query->kd_dana,
-                'name'  => $query->studyProgram->nama.' - '.$query->academicYear->tahun_akademik,
-                'url'   => route('funding.study-program.show',encrypt($query->kd_dana))
+                'name'  => $query->studyProgram->nama . ' - ' . $query->academicYear->tahun_akademik,
+                'url'   => route('funding.study-program.show', encrypt($query->kd_dana))
             ];
-            $this->log('updated','Keuangan Program Studi',$property);
+            $this->log('updated', 'Keuangan Program Studi', $property);
 
             DB::commit();
-            return redirect()->route('funding.study-program.show',$request->_id)->with('flash.message', 'Data berhasil disunting!')->with('flash.class', 'success');
-        } catch(\Exception $e) {
+            return redirect()->route('funding.study-program.show', $request->_id)->with('flash.message', 'Data berhasil disunting!')->with('flash.class', 'success');
+        } catch (\Exception $e) {
             DB::rollback();
             return redirect()->back()->with('flash.message', $e->getMessage())->with('flash.class', 'danger')->withInput($request->input());
         }
-
     }
 
 
     public function destroy(Request $request)
     {
-        if(!$request->ajax()) {
+        if (!$request->ajax()) {
             abort(404);
         }
 
@@ -221,15 +218,15 @@ class FundingStudyProgramController extends Controller
             $id = decrypt($request->id);
 
             //Query
-            $data = FundingStudyProgram::where('kd_dana',$id)->first();
-            FundingStudyProgram::where('kd_dana',$id)->delete();
+            $data = FundingStudyProgram::where('kd_dana', $id)->first();
+            FundingStudyProgram::where('kd_dana', $id)->delete();
 
             //Activity Log
             $property = [
                 'id'    => $data->kd_dana,
-                'name'  => $data->faculty->nama.' - '.$data->academicYear->tahun_akademik,
+                'name'  => $data->faculty->nama . ' - ' . $data->academicYear->tahun_akademik,
             ];
-            $this->log('deleted','Keuangan Fakultas',$property);
+            $this->log('deleted', 'Keuangan Fakultas', $property);
 
             DB::commit();
             return response()->json([
@@ -237,11 +234,11 @@ class FundingStudyProgramController extends Controller
                 'message' => 'Data berhasil dihapus',
                 'type'    => 'success'
             ]);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             DB::rollback();
             return response()->json([
                 'message' => $e->getMessage(),
-            ],400);
+            ], 400);
         }
     }
 }
