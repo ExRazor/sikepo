b77a732ad9e8028407c64cb96c951a5c77bd85cb
ExRazor <keroro.gunso97@gmail.com>
2021-02-19 03:14:28 +0800

    Tambahkan seeder untuk demo sementara

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,22 +28,22 @@ class DatabaseSeeder extends Seeder
         $this->call(CurriculumSeeder::class);
         $this->call(StudentSeeder::class);
 
-        // $this->call(StudentStatusSeeder::class);
-        // $this->call(CollaborationSeeder::class);
-        // $this->call(TeacherAchievementSeeder::class);
-        // $this->call(CurriculumScheduleSeeder::class);
-        // $this->call(StudentQuotaSeeder::class);
-        // $this->call(StudentForeignSeeder::class);
-        // $this->call(StudentAchievementSeeder::class);
-        // $this->call(FundFacultySeeder::class);
-        // $this->call(FundStudyProgramSeeder::class);
+        $this->call(StudentStatusSeeder::class);
+        $this->call(CollaborationSeeder::class);
+        $this->call(TeacherAchievementSeeder::class);
+        $this->call(CurriculumScheduleSeeder::class);
+        $this->call(StudentQuotaSeeder::class);
+        $this->call(StudentForeignSeeder::class);
+        $this->call(StudentAchievementSeeder::class);
+        $this->call(FundFacultySeeder::class);
+        $this->call(FundStudyProgramSeeder::class);
         $this->call(ResearchSeeder::class);
         $this->call(ResearchTeacherSeeder::class);
         $this->call(ResearchStudentsSeeder::class);
         $this->call(CommunityServiceSeeder::class);
         $this->call(CommunityServiceTeacherSeeder::class);
         $this->call(CommunityServiceStudentsSeeder::class);
-        // $this->call(EwmpSeeder::class);
+        $this->call(EwmpSeeder::class);
         $this->call(PublicationSeeder::class);
         $this->call(PublicationMemberSeeder::class);
         // $this->call(TeacherPublicationSeeder::class);
@@ -51,14 +51,14 @@ class DatabaseSeeder extends Seeder
         // $this->call(TeacherPublicationStudentSeeder::class);
         // $this->call(StudentPublicationSeeder::class);
         // $this->call(StudentPublicationMemberSeeder::class);
-        // $this->call(TeacherOutputActivity::class);
-        // $this->call(StudentOutputActivity::class);
-        // $this->call(MinithesisSeeder::class);
-        // $this->call(CurriculumIntegrationSeeder::class);
-        // $this->call(AcademicSatisfactionSeeder::class);
-        // $this->call(AlumnusSatisfactionSeeder::class);
-        // $this->call(AlumnusIdleSeeder::class);
-        // $this->call(AlumnusSuitableSeeder::class);
-        // $this->call(AlumnusWorkplaceSeeder::class);
+        $this->call(TeacherOutputActivity::class);
+        $this->call(StudentOutputActivity::class);
+        $this->call(MinithesisSeeder::class);
+        $this->call(CurriculumIntegrationSeeder::class);
+        $this->call(AcademicSatisfactionSeeder::class);
+        $this->call(AlumnusSatisfactionSeeder::class);
+        $this->call(AlumnusIdleSeeder::class);
+        $this->call(AlumnusSuitableSeeder::class);
+        $this->call(AlumnusWorkplaceSeeder::class);
     }
 }
